c7945dcf0d0cd4546c00b9ed22d9ef0fc2f328e7
dimassdoubles <codedimas@gmail.com>
2024-02-18 21:12:59 +0700

    Initial Commit

---
+++ DynamicGradientBorderAnimationsOnCards/DynamicGradientBorderAnimationsOnCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicGradientBorderAnimationsOnCardsApp.swift
+//  DynamicGradientBorderAnimationsOnCards
+//
+//  Created by Gatotkaca on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicGradientBorderAnimationsOnCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
